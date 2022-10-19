0171803958be74867f3ebe824e61013121b0bd21
zlfera <zlfera@gmail.com>
2022-10-19 15:10:32 +0800

    add interface User

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+// import { AxiosRequestConfig } from "axios";
+
+// declare module "axios" {
+//     export interface AxiosInstance {
+//         <T = any>(config: AxiosRequestConfig): Promise<T>;
+//         request<T = any>(config: AxiosRequestConfig): Promise<T>;
+//         get<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>;
+//         delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>;
+//         head<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>;
+//         post<T = any>(
+//             url: string,
+//             data?: any,
+//             config?: AxiosRequestConfig
+//         ): Promise<T>;
+//         put<T = any>(
+//             url: string,
+//             data?: any,
+//             config?: AxiosRequestConfig
+//         ): Promise<T>;
+//         patch<T = any>(
+//             url: string,
+//             data?: any,
+//             config?: AxiosRequestConfig
+//         ): Promise<T>;
+//     }
+// }
+declare interface ImportEnv {
+    VITE_API: string;
+}
+export interface User {
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+}
